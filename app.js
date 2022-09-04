f11424ddb94233a2211a17dfddfd35cb2a867748
Dmitriy Fomin <fomich.dm@yandex.ru>
2022-09-04 20:44:02 +0300

    cors

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(cors());
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 app.use(express.json());
-app.use(cors());
 
 app.use(requestLogger);
 
